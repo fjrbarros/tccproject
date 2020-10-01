8f5985c01a06862d3bec8cbee89771067461ffce
Fabio Junior B. Barbosa <fabio.barbosa@fabioBarbosa.useall.corp>
2020-09-30 22:18:45 -0300

    Changes chart

--- src/components/chart/Index.jsx
+++ src/components/chart/Index.jsx
@@ -1,81 +1,113 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useEffect, useRef, useState } from 'react';
 import { useStyles } from './Style';
+import { select, scaleLinear, scaleBand, max, axisLeft, axisBottom } from 'd3';
 
 function Chart(props) {
     const classes = useStyles();
     const ref = useRef();
+    const [showInfo, setShowInfo] = useState(false);
 
     useEffect(() => { generateSvg(); }, []);
 
-    // data = [{
-    //     ano: 2019,
-    //     atividades: [{
-    //         comentarios: [],
-    //         dataInicio: null,
-    //         dataPrevistaInicio: "04/12/2019",
-    //         dataPrevistaTermino: "16/01/2020",
-    //         dataTermino: null,
-    //         descricao: "Título",
-    //         detalhes: null,
-    //         estagio: "TO_DO",
-    //         id: 353,
-    //         percentualConclusao: 0
-    //     }]
-    // }, {
-    //     ano: 2020,
-    //     atividades: [{
-    //         comentarios: [],
-    //         dataInicio: null,
-    //         dataPrevistaInicio: "04/12/2019",
-    //         dataPrevistaTermino: "16/01/2020",
-    //         dataTermino: null,
-    //         descricao: "Título",
-    //         detalhes: null,
-    //         estagio: "TO_DO",
-    //         id: 353,
-    //         percentualConclusao: 0
-    //     }]
-    // }]
-
     function generateSvg() {
-        const svgElement = d3.select(ref.current);
+        const data = [{
+            country: 'teste1', population: '123'
+        }, {
+            country: 'teste2', population: '456'
+        }, {
+            country: 'teste3', population: '789'
+        }, {
+            country: 'teste4', population: '120'
+        }, {
+            country: 'teste5', population: '450'
+        }, {
+            country: 'teste6', population: '790'
+        }, {
+            country: 'teste7', population: '30'
+        }, {
+            country: 'teste8', population: '50'
+        }, {
+            country: 'teste9', population: '80'
+        }, {
+            country: 'teste10', population: '123'
+        }, {
+            country: 'teste11', population: '456'
+        }, {
+            country: 'teste12', population: '789'
+        }];
 
-        const data = [30, 15, 50, 70, 80];
+        data.forEach(d => {
+            d.population = d.population * 10;
+        });
 
-        const width = 500;
-        const height = 500;
+        const svgElement = select(ref.current);
+        const width = svgElement.attr('width');
+        const height = svgElement.attr('height');
+        const margin = { top: 20, right: 20, bottom: 20, left: 50 };
+        const innerWidth = width - margin.left - margin.right;
+        const innerHeight = height - margin.top - margin.bottom;
+        const xScale = scaleLinear()
+            .domain([0, max(data, d => d.population)])
+            .range([0, innerWidth]);
+        const yScale = scaleBand()
+            .domain(data.map(d => d.country))
+            .range([0, innerHeight])
+            .padding(0.1)
 
-        // svgElement.attr('width', width)
-        //     .attr('height', height);
+        const g = svgElement.append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-        const widthScale = d3.scaleLinear()
-            .domain([0, 80])
-            .range([0, width]);
+        g.append('g').call(axisLeft(yScale));
+        g.append('g').call(axisBottom(xScale))
+            .attr('transform', `translate(0, ${innerHeight})`);
 
-        const colorScale = d3.scaleLinear()
-            .domain([0, 80])
-            .range(['red', 'blue']);
-
-        const bars = svgElement.selectAll('rect')
+        g.selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
+            .attr('y', d => yScale(d.country))
             .attr('width', 0)
-            .attr('height', 30)
-            .attr('fill', d => colorScale(d))
-            .attr('y', (d, i) => i * 35)
-
-        bars.transition()
+            .attr('height', yScale.bandwidth())
+            .transition()
             .duration(1000)
-            .attr('width', d => widthScale(d))
+            .attr('width', d => xScale(d.population))
 
+        const tooltip = svgElement.append('g')
+            .attr('class', classes.tooltip)
+            .style('display', 'none')
 
+        tooltip.append('text')
+            .attr('x', 15)
+            .attr('dy', '1.e2m')
+            .style('font-size', '1.25em')
+            .attr('font-weight', 'bold')
+
+        const bars = svgElement.selectAll('rect');
+
+        bars.on('mouseover', (event, data) => {
+            // tooltip.style('display', 'block');
+            setShowInfo(false);
+        });
+
+        bars.on('mouseout', (event, data) => {
+            tooltip.style('display', 'none');
+            setShowInfo(false);
+        });
+
+        bars.on('mousemove', (event, d) => {
+            const xPos = event.clientX - 10;
+            const yPos = event.clientY - 105;
+            tooltip.attr('transform', `translate(${xPos}, ${yPos})`);
+            tooltip.select('text').text(d.country);
+            tooltip.style('display', 'block');
+            setShowInfo(true);
+        });
     }
 
     return (
         <div className={classes.content}>
-            <svg ref={ref} className={classes.svg} />
+            <svg ref={ref} className={classes.svg} width='960' height='500' />
+            {showInfo && <div>asdasdfsafasfasfsafasfas</div>}
         </div>
     );
 }
--- src/components/chart/Style.jsx
+++ src/components/chart/Style.jsx
@@ -3,12 +3,25 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(() => ({
     svg: {
         height: '100%',
-        width: '100%'
+        width: '100%',
+        '& rect': {
+            fill: 'green'
+        },
+        '& rect:hover': {
+            fill: 'red'
+        },
+        '& text': {
+            fontSize: '1.4em'
+        }
     },
 
     content: {
         width: '100%',
         height: '100%',
-        // backgroundColor: 'tomato'
+        display: 'flex'
+    },
+
+    tooltip: {
+        color: 'blue'
     }
 }))
